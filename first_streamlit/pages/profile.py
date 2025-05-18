cddf4a5fde9a71edb9a6cbd52b5a46daa3e3087a
suprechath <suprechathongsakulpanich@gmail.com>
2025-05-18 15:04:15 +0700

    first2

--- first_streamlit/pages/profile.py
+++ first_streamlit/pages/profile.py
@@ -1,8 +1,9 @@
 import streamlit as st
 
 # Main page content
-st.markdown("# Main page 🎈")
-st.sidebar.markdown("# Main page 🎈")
+st.markdown("# Profile 🎈")
+st.sidebar.markdown("# Profile 🎈")
+st.write("Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.")
 
 # if ('name' not in st.session_state):
 #     st.session_state.name = []
--- first_streamlit/streamlit_app.py
+++ first_streamlit/streamlit_app.py
@@ -1,10 +1,23 @@
 import streamlit as st
+import multipage_streamlit as mt
+from pages import cir, hist, profile, rec
+
+app = mt.MultiPage()
+app.add("Profile", profile.run)
+app.add("Circle calculation", cir.run)
+app.add("Rectangular calculation", rec.run)
+app.add("Histrogram", hist.run)
+
+style = st.sidebar.radio("MultiPage style:", app.styles,
+    help="Choose the style of the multipage app")
+
+app.run(style)
 
 # Define the pages
-main_page = st.Page("pages/profile.py", title="Main Page", icon="🎈")
-page_2 = st.Page("pages/1_cal_cir_250518.py", title="Circle", icon="❄️")
-page_3 = st.Page("pages/2_cal_regtang_250518.py", title="Rectangular", icon="🎉")
-histrogram = st.Page("pages/Histogram.py", title="Histrogram", icon="🎈")
+# main_page = st.Page("pages/profile.py", title="Main Page", icon="🎈")
+# page_2 = st.Page("pages/1_cal_cir_250518.py", title="Circle", icon="❄️")
+# page_3 = st.Page("pages/2_cal_regtang_250518.py", title="Rectangular", icon="🎉")
+# histrogram = st.Page("pages/Histogram.py", title="Histrogram", icon="🎈")
 
 
 # Set up navigation
